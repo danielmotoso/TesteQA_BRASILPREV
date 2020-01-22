aecb41164fe49b646a7cdd0acdfa8c9b2a42f0f2
Daniel Motoso <danielmotoso@hotmail.com>
2020-01-22 10:59:21 -0300

    Alteração na Classe ArquivoJSON

--- src/test/java/ArquivoJSON.java
+++ src/test/java/ArquivoJSON.java
@@ -1,4 +1,25 @@
+public class ArquivoJSON
+{
 
+  public String trazerArquivo(String dd, String telefone, String cpf) {
+       String json = "{ \"codigo\": 0, \n" +
+                "\"nome\": \"Daniel Silva Motoso\", \n" +
+                "\"cpf\": \"" + cpf + "\", \n" +
+                "\"enderecos\": [ { \n" +
+                "\t\"logradouro\": \"Daniel Silva Motoso\",\n" +
+                "\t\"numero\": 123, \n" +
+                "\t\"complemento\": \"Empresa\",\n" +
+                "\t\"bairro\": \"Chacara Santo Antonio\", \n" +
+                "\t\"cidade\": \"São Paulo\", \n" +
+                "\t\"estado\": \"SP\" } ], \n" +
+                "\"telefones\": [ { \n" +
+                "\t\"ddd\": \"" + dd + "\",\n" +
+                "\t\"numero\": \"" + telefone + "\"}] }";
+       return json;
+    }
+}
+
+/*
 public class ArquivoJSON {
     private String dd;
     private String telefone;
@@ -53,6 +74,6 @@ public class ArquivoJSON {
     }
 
 }
-
+*/
 
 
